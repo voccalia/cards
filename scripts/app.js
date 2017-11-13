99323808dfefaee73466b2e75f93f9341f1c012e
Sebastian Serna <jsebastianserna@gmail.com>
2017-11-12 22:34:40 -0500

    Prueba de Vue.js con Axios

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+var dataCharacters = '/data/characters.json';
+new Vue({
+    el: '#characters',
+    created: function() {
+        this.getCharacters();
+    },
+    data: {
+        characters: []
+    },
+    methods: {
+        getCharacters: function() {
+            axios.get(dataCharacters).then(response => {
+                this.characters = response.data
+            });
+        }
+    }
+});
